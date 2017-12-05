f91b38642148577af43ca9d98343572724df28a9
mnoskov <nm@perm-tools.ru>
2017-12-05 14:49:59 +0500

    refactored

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -5,11 +5,11 @@ namespace App\Controllers;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-class HomeController extends Controller {
-
-    public function index(Request $request, Response $response, array $args): Response {
-        $this->view->render($response, 'home.twig');
-        return $response;
+class HomeController extends Controller
+{
+    public function index(Request $request, Response $response, array $args): Response
+    {
+        return $this->view->render($response, 'home.twig');
     }
 
 }
