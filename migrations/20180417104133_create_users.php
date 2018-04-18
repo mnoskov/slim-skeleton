65a5753cc9b0da4c5c24aaf263b7978db76916a8
Mikhail Noskov <nm@perm-tools.ru>
2018-04-18 06:25:04 +0000

    users migration added

---
+++ migrations/20180417104133_create_users.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use \App\System\Migration;
+
+class CreateUsers extends Migration
+{
+    public function up()
+    {
+        $this->schema->create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('login', 32);
+            $table->string('password');
+            $table->timestamps();
+            $table->index('login');
+        });
+    }
+
+    public function down()
+    {
+        $this->schema->drop('users');
+    }
+}
