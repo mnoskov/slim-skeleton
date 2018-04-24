dd421c36d401908a0e301719b3cac20ace705b4c
Mikhail Noskov <nm@perm-tools.ru>
2018-04-24 10:25:16 +0000

    base controller moved to system folder

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Controllers;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-class HomeController extends Controller
+class HomeController extends \App\System\Controller
 {
     public function index(Request $request, Response $response, array $args): Response
     {
