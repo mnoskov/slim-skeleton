d6652341158d63705302ba4b5110c3c59c0d58e2
Mikhail Noskov <nm@perm-tools.ru>
2018-07-03 10:32:33 +0000

    flash, old input, validation

---
+++ app/Middleware/OldInputMiddleware.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Middleware;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class OldInputMiddleware extends Middleware
+{
+    public function __invoke(Request $request, Response $response, callable $next)
+    {
+        if ($request->getMethod() == 'POST') {
+            $this->ci->flash->addMessage('old', $request->getParams());
+        }
+
+        return $next($request, $response);
+    }
+}
--- app/dependencies.php
+++ app/dependencies.php
@@ -1,16 +1,31 @@
 <?php
 
-$container['view'] = function($c) {
+$container['view'] = function($c)
+{
     $settings = $c->get('settings');
     $view = new \Slim\Views\Twig($settings['view']['template_path'], $settings['view']['twig']);
 
     $view->addExtension(new Slim\Views\TwigExtension($c->get('router'), $c->get('request')->getUri()));
     $view->addExtension(new Twig_Extension_Debug());
 
+    $env = $view->getEnvironment();
+
+    $env->addGlobal('app', $settings['app']);
+    $env->addGlobal('flash', $c->flash->getMessages());
+
+    if ($c->flash->hasMessage('old')) {
+        $env->addGlobal('old', $c->flash->getFirstMessage('old'));
+    }
+
+    $env->addFilter(new Twig_Filter('if_undefined', function($value, $default) {
+        return $value === null ? $default : $value;
+    }));
+
     return $view;
 };
 
-$container['logger'] = function ($c) {
+$container['logger'] = function ($c)
+{
     $settings = $c->get('settings')['logger'];
     $logger = new \Monolog\Logger($settings['name']);
     $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
@@ -24,7 +39,8 @@ $capsule->addConnection($settings);
 $capsule->bootEloquent();
 $capsule->setAsGlobal();
 
-$container['db'] = function($c) use ($capsule, $settings) {
+$container['db'] = function($c) use ($capsule, $settings)
+{
     $db = $capsule->getConnection();
     if ($settings['log']) {
         $db->enableQueryLog();
@@ -32,8 +48,31 @@ $container['db'] = function($c) use ($capsule, $settings) {
     return $db;
 };
 
-$container['mailer'] = function($c) {
+$container['mailer'] = function($c)
+{
+    if (!class_exists('Swift_SendmailTransport')) {
+        throw new Exception('Mailer class not found! Run "composer require swiftmailer/swiftmailer" in console.');
+    }
+
     $transport = new Swift_SendmailTransport('/usr/sbin/sendmail -bs');
     return new Swift_Mailer($transport);
 };
 
+$container['flash'] = function($c)
+{
+    return new Slim\Flash\Messages;
+};
+
+$container['validator'] = function($c)
+{
+    if (!class_exists('Illuminate\Validation\Factory')) {
+        throw new Exception('Validation class not found! Run "composer require illuminate/validation" in console.');
+    }
+
+    $filesystem = new Illuminate\Filesystem\Filesystem();
+    $fileLoader = new Illuminate\Translation\FileLoader($filesystem, '');
+    $translator = new Illuminate\Translation\Translator($fileLoader, 'en_US');
+    $validator  = new Illuminate\Validation\Factory($translator);
+
+    return $validator;
+};
